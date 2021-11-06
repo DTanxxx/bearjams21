88392a724a71f86a029f85a17493232b509b3fa4
Sophia Chang <sophchang21@gmail.com>
2021-11-06 11:59:46 -0700

    pushed basic movement and placeholder assets

---
+++ inconsistent/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float speed;
+    public bool onGround;
+    public float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(x * speed, body.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            onGround = false;
+        }
+    }
+}

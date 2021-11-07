711942855c8d2efc8fb0398abf878a7a299dcab2
David Tan <tanweimin2013@gmail.com>
2021-11-07 00:44:17 -0700

    Added_Control_Inconsistencies_and_destroyer_block_forcing_player_movement

--- inconsistent/Assets/Scripts/PlayerScript.cs
+++ inconsistent/Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D myFeet = null;
+    [SerializeField] private KeyCode[] keyCodes = new KeyCode[3];
 
     private Rigidbody2D body;
     private BoxCollider2D myCollider;
@@ -16,30 +17,169 @@ public class PlayerScript : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<BoxCollider2D>();
+        keyCodes[0] = KeyCode.LeftArrow;
+        keyCodes[1] = KeyCode.UpArrow;
+        keyCodes[2] = KeyCode.RightArrow;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-        Jump();
         Die();
     }
 
-    void Move()
+    private void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(x * speed, body.velocity.y);
-    }
-
-    void Jump()
-    {
-        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
-            !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms"))) { return; }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (keyCodes[0] == KeyCode.LeftArrow && keyCodes[2] == KeyCode.RightArrow)
         {
-            Debug.Log("Player jumps!");
-            body.velocity += new Vector2(body.velocity.x, jumpHeight);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                body.velocity = new Vector2(-speed, body.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(0, body.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
+                !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+                {
+                    return;
+                }
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            }
+        }
+        else if (keyCodes[0] == KeyCode.RightArrow && keyCodes[2] == KeyCode.LeftArrow)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                body.velocity = new Vector2(-speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(0, body.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
+                !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+                {
+                    return;
+                }
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            }
+        }
+        else if (keyCodes[0] == KeyCode.LeftArrow && keyCodes[1] == KeyCode.RightArrow)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                body.velocity = new Vector2(-speed, body.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(0, body.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
+                !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+                {
+                    return;
+                }
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            }
+        }
+        else if (keyCodes[0] == KeyCode.RightArrow && keyCodes[1] == KeyCode.LeftArrow)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                body.velocity = new Vector2(-speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(0, body.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
+                !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+                {
+                    return;
+                }
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            }
+        }
+        else if (keyCodes[1] == KeyCode.LeftArrow && keyCodes[2] == KeyCode.RightArrow)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                body.velocity = new Vector2(-speed, body.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(0, body.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
+                !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+                {
+                    return;
+                }
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            }
+        }
+        else if (keyCodes[1] == KeyCode.RightArrow && keyCodes[2] == KeyCode.LeftArrow)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                body.velocity = new Vector2(-speed, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(0, body.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) &&
+                !myFeet.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+                {
+                    return;
+                }
+                body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            }
         }
     }
 
@@ -50,5 +190,29 @@ public class PlayerScript : MonoBehaviour
         {
             FindObjectOfType<GameSession>().ProcessPlayerDeath();
         }
+    }
+
+    public void SwitchControl()
+    {
+        Debug.Log("SWITCH!");
+        KeyCode[] temp = new KeyCode[3];
+        int randomIndex = Random.Range(0, 2);
+        temp[0] = keyCodes[randomIndex];
+        temp[randomIndex] = keyCodes[0];
+        
+        if (randomIndex == 1)
+        {
+            temp[2] = keyCodes[2];
+        }
+        else if (randomIndex == 2)
+        {
+            temp[1] = keyCodes[1];
+        }
+        else
+        {
+            temp[1] = keyCodes[2];
+            temp[2] = keyCodes[1];
+        }
+        keyCodes = temp;
     }
 }

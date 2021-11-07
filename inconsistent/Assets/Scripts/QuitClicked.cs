028c6f30ecf0dc55e3f4ca34301a719818969872
David Tan <tanweimin2013@gmail.com>
2021-11-07 14:04:50 -0800

    Start_menu

---
+++ inconsistent/Assets/Scripts/QuitClicked.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitClicked : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Application.Quit(0);
+    }
+}

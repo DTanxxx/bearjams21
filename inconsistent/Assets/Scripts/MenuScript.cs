db0bb944c9c9bf5cce9ac5b95a85bbd1342d10f5
Sophia Chang <sophchang21@gmail.com>
2021-11-07 14:35:22 -0800

    basic pause functionality added to all three levels

---
+++ inconsistent/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	GameObject[] pauseObjects;
+	GameObject[] finishObjects;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Time.timeScale = 1;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//uses the p button to pause and unpause the game
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+			}
+			else if (Time.timeScale == 0)
+			{
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+	//Reloads the Level
+	public void Reload()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	//loads inputted level
+	public void LoadLevel(string level)
+	{
+		SceneManager.LoadScene(level);
+	}
+}
---
+++ inconsistent/Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Time.timeScale = 1;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//uses the p button to pause and unpause the game
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				AudioListener.pause = true;
+			}
+			else if (Time.timeScale == 0)
+			{
+				Debug.Log("unpause");
+				Time.timeScale = 1;
+				AudioListener.pause = false;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			Restart();
+		}
+	}
+
+	// Restarts current level
+	public void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	// load other level
+	public void LoadLevel(string level)
+	{
+		SceneManager.LoadScene(level);
+	}
+}
